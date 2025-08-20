36939ebe82ba11715ffe52afcf8a0dc64e59eaae
davyidx <deasdavid9@gmail.com>
2025-08-19 21:14:50 -0300

    install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Variables
+BIN_FILE="ezs"
+SHARE_DIR="/usr/share/ezs/"
+SHARE_FILE1=".default_cursor"
+SHARE_FILE2=".default_session"
+SERVICE_FILE="ezs.service"
+
+if [ ! -d "$SHARE_DIR" ]; then
+    sudo mkdir -p "$SHARE_DIR"
+fi
+
+sudo cp "$BIN_FILE" /usr/bin/
+sudo chmod +x "/usr/bin/$BIN_FILE"
+
+sudo cp "$SHARE_FILE1" "$SHARE_DIR/"
+sudo cp "$SHARE_FILE2" "$SHARE_DIR/"
+
+sudo cp "$SERVICE_FILE" /etc/systemd/system/
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable "$SERVICE_FILE"
+
+echo "Completed."
